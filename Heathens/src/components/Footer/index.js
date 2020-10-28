2445e7a70ce4c37ce4bab7e79b97a4a50631e3f2
Gabriel-ark <73611683+Gabriel-ark@users.noreply.github.com>
2020-10-28 19:32:58 -0300

    Add files via upload

---
+++ Heathens/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <p>
+        Orgulhosamente criado durante a Pandemia
+        {' '}
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
